bc12ea09896fb2e3b7d0ba036ab5a05a3914c89b
Iroshan Vithanage <iroshanav@gmail.com>
2017-05-19 10:22:35 +0530

    commented

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'xxx');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'xxx');
+   define('DB_SERVER', 'localhost'); /* HOST */
+   define('DB_USERNAME', 'xxx');     /* USERNAME */
+   define('DB_PASSWORD', '');        /* PASSWORD */
+   define('DB_DATABASE', 'xxx');     /* DATABASE NAME */
    $con = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
